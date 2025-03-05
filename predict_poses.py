eb473367c810f5a083282e10c15f1eaaf7731a97
tongyu <119610311+tongyu0924@users.noreply.github.com>
2025-03-05 18:47:18 +0800

    Update predict_poses.py

--- predict_poses.py
+++ predict_poses.py
@@ -8,10 +8,11 @@ import torch.nn as nn
 import os
 import numpy as np
 from tqdm import tqdm
+from timesformer.models.swin_vit import SwinTransformer3D
 
 
-checkpoint_path = "checkpoints/Exp3"
-checkpoint_name = "checkpoint_e90"
+checkpoint_path = "/home/leohsu/tongyu/TSformer-VO/checkpoints/Exp_video_swin_transformer"
+checkpoint_name = "checkpoint_e80"
 sequences = ["01", "03", "04", "05", "06", "07", "10"]
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -34,20 +35,21 @@ preprocess = transforms.Compose([
 ])
 
 # build and load model
-model = VisionTransformer(img_size=model_params["image_size"],
-                          num_classes=model_params["num_classes"],
-                          patch_size=model_params["patch_size"],
-                          embed_dim=model_params["dim"],
-                          depth=model_params["depth"],
-                          num_heads=model_params["heads"],
-                          mlp_ratio=4,
-                          qkv_bias=True,
-                          norm_layer=partial(nn.LayerNorm, eps=1e-6),
-                          drop_rate=0.,
-                          attn_drop_rate=0.,
-                          drop_path_rate=0.1,
-                          num_frames=model_params["num_frames"],
-                          attention_type=model_params["attention_type"])
+model = SwinTransformer3D(
+        img_size=model_params["image_size"],
+        num_classes=model_params["num_classes"],
+        depth=model_params["depth"],
+        num_frames=model_params["num_frames"],
+        # num_heads=model_params["heads"],  # Ensure this is a list/tuple
+        window_size=(2, 7, 7),
+        embed_dim=model_params["dim"],
+        mlp_ratio=4.0,
+        qkv_bias=True,
+        drop_rate=0.0,
+        attn_drop_rate=model_params["attn_dropout"],
+        drop_path_rate=model_params["ff_dropout"],
+        norm_layer=nn.LayerNorm,
+    )
 
 checkpoint = torch.load(os.path.join(args["checkpoint_path"], "{}.pth".format(checkpoint_name)),
                         map_location=torch.device(device))
@@ -68,16 +70,16 @@ for sequence in sequences:
     with tqdm(test_loader, unit="batch") as batchs:
       pred_poses = torch.zeros((1, args["window_size"] - 1, 6), device=device)
       batchs.set_description(f"Sequence {sequence}")
-      for images, gt in batchs:
+      for images, depth, flow, gt  in batchs:
         if torch.cuda.is_available():
-          images, gt = images.cuda(), gt.cuda()
+          images, depth, flow, gt = images.cuda(), depth.cuda(), flow.cuda(), gt.cuda()
 
           with torch.no_grad():
             model.eval()
             model.training = False
 
             # predict pose
-            pred_pose = model(images.float())
+            pred_pose = model(images.float(), depth.float(), flow.float())
             pred_pose = torch.reshape(pred_pose, (args["window_size"] - 1, 6)).to(device)
             pred_pose = pred_pose.unsqueeze(dim=0)
             pred_poses = torch.concat((pred_poses, pred_pose), dim=0)
